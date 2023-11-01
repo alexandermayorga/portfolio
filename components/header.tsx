8a8cd061eda2989f073d8329420117835d8f843f
Alexander Mayorga <alexandermayorga@users.noreply.github.com>
2023-11-01 17:53:40 -0500

    Initial Commit

---
+++ components/header.tsx
@@ -0,0 +1,41 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="/" className="fs-3">
+            <span className="text-lemongrass">{"{ "}</span>
+            <span className="text-sky me-1">a</span>
+            <span className="text-magenta">m</span>
+            <span className="text-lime">{" }"}</span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="#link">Projects</Nav.Link>
+              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
