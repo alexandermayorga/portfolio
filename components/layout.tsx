22ffa4f92fa54071f338c25c721ab37b1317b920
Alexander Mayorga <alexandermayorga@users.noreply.github.com>
2023-11-10 19:42:41 -0500

    Fix: Adding Google Analytics the Next.js way as per documentation

--- components/layout.tsx
+++ components/layout.tsx
@@ -16,13 +16,20 @@ export default function Layout({ children }: { children: ReactNode }) {
           name="description"
           content="Alex Mayorga | Full Stack JavaScript Developer"
         />
-        <meta
-          property="og:image"
-          content="/alexmayorga-logo-og-image.jpeg"
-        />
+        <meta property="og:image" content="/alexmayorga-logo-og-image.jpeg" />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-GV7QF43GS8" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-GV7QF43GS8');
+        `}
+      </Script>
       {/* <Script strategy="lazyOnload" src='https://unpkg.com/holderjs@2.9.9/holder.js'></Script> */}
       <Header></Header>
       <main>{children}</main>
