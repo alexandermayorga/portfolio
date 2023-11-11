39d6943a11834f7f8a224bb0f6aa23ce8d074812
Alexander Mayorga <alexandermayorga@users.noreply.github.com>
2023-11-10 19:33:59 -0500

    Add Third Party Script: Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 // import '@/styles/globals.css'
 import "@/styles/main.scss";
+import Head from "next/head";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        id="show-banner"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
